4a16efe1e90a5e988a9fba9674b543b91c4f8b95
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2023-12-24 23:34:08 +0530

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -18,5 +18,39 @@
 }
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2. Search Insert Position
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) end=mid-1;
+            else start=mid+1;
+        }
+        return start;
+    }
+}
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 3. Happy Number
+
+class Solution {
+    public boolean isHappy(int n) {
+       Set<Integer> set=new HashSet<>();
+       while(n!=1){
+           int c=n;
+           int s=0;
+           while(c>0){
+               s+=(c%10)*(c%10);
+               c/=10;
+           }
+           if(!set.add(s)){
+               return false;
+           }
+           n=s;
+       }
+       return true;
+    }
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
