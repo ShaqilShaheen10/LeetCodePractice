986ee5c5dfdbbd50d4faa301dbc207a86e94429f
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2023-12-29 19:48:02 +0530

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -82,5 +82,32 @@ class Solution {
     }
 }
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-5.
+5.  Swap Nodes in Pairs
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode t=new ListNode(0);
+        t.next=head;
+        ListNode cur=t;
+        while(cur.next!=null && cur.next.next!=null){
+            ListNode first=cur.next;
+            ListNode second=cur.next.next;
+            first.next=second.next;
+            cur.next=second;
+            cur.next.next=first;
+            cur=cur.next.next;
+        }
+        return t.next;
+    }
+}
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
