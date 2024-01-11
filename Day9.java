66eff99a4f1a4563f90eb3b1630d3c9c0fb05aba
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-01-11 13:18:02 +0530

    Update Day9.java

--- Day9.java
+++ Day9.java
@@ -134,4 +134,23 @@ class Solution {
     }
 }
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+5 Longest Substring Without Repeating Characters
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        int m=0;
+        int[] charIndex=new int[128];
+        Arrays.fill(charIndex,-1);
+        int l=0;
+        for(int r=0;r<n;r++){
+            if(charIndex[s.charAt(r)]>=l){
+                l=charIndex[s.charAt(r)]+1;
+            }
+            charIndex[s.charAt(r)]=r;
+            m=Math.max(m,r-l+1);
+        }
+        return m;
+    }
+}
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
