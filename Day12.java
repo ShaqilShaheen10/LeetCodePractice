6910e53c65eb035183b4c8cfb226c43c75025a85
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-01-24 14:10:55 +0530

    Update Day12.java

--- Day12.java
+++ Day12.java
@@ -34,7 +34,38 @@ class Solution {
     }
 }
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-3.
+3. Pseudo-Palindromic Path in a Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pseudoPalindromicPaths (TreeNode root) {
+      return countP(root,0);
+    }
+    public int countP(TreeNode node,int path){
+        if(node==null){
+            return 0;
+        }
+        path^=(1<<node.val);
+        if(node.left==null && node.right==null){
+            return (path&(path-1))==0?1:0;
+        }
+        return countP(node.left,path)+countP(node.right,path);
+    }
+}
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 4.
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
