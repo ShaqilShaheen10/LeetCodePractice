1fbbc4edad5e8b270b7717738ccf0d2b57f43137
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-03-15 09:47:58 +0530

    Update Day21.java

--- Day21.java
+++ Day21.java
@@ -20,16 +20,14 @@ class Solution {
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2. Middle of The Linked List
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class Solution {
     public ListNode middleNode(ListNode head) {
         if(head==null || head.next==null)return head;
