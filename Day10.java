2da411f0b7a3e027d9a58e377a7ab03944d9fed3
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-04-02 10:33:04 +0530

    Update Day10.java

--- Day10.java
+++ Day10.java
@@ -70,18 +70,15 @@ class Solution {
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 4. Linked List Cycle
 
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode s=head,f=head;
         while(f!=null && f.next!=null){
