0f1076f83213420ac9701c2f678bb80aae4d8620
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-04-06 13:19:56 +0530

    Update Day13.java

--- Day13.java
+++ Day13.java
@@ -18,21 +18,19 @@ class Solution {
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2. Count Complete Tree Nodes
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
 class Solution {
     public int countNodes(TreeNode root) {
     return root==null?0:1+countNodes(root.left)+countNodes(root.right);
