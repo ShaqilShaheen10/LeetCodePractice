40b3c5b442d0d6d7b4451dd15c2cb81c75142087
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-04-13 22:46:33 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -67,22 +67,20 @@ class Solution {
 }
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 4. Symmetric Tree
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+ 
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
 class Solution {
    public boolean isSymmetric(TreeNode root){
        if(root==null){
