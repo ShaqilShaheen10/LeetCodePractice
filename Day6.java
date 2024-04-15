47084e29b980f6a8021a5bc79d07b2c61d9e05ea
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-04-15 11:01:02 +0530

    Update Day6.java

--- Day6.java
+++ Day6.java
@@ -31,22 +31,19 @@ class Solution {
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2. Binary Tree PreOrder Traversal
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
       List<Integer> ans=new ArrayList<>();
       helper(root,ans);
