8ba6df08b2125c70038f838490ba7a9c6f5c336a
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-04-27 12:33:16 +0530

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -26,16 +26,14 @@ class Solution{
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2. Merge Two Sorted Lists
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+   public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1!=null && list2!=null){
