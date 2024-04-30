bf40893f67fc20c6f0eb3a3c939664223ae35a57
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-04-30 10:38:59 +0530

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -54,21 +54,19 @@ class Solution {
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 3. Binary Tree Inorder Traversal
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
       List<Integer> res=new ArrayList<>();
