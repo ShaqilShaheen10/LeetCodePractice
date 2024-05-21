fe0a28606de0a1496298df80836ecb247d524979
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-05-21 10:57:06 +0530

    Update Day23.java

--- Day23.java
+++ Day23.java
@@ -3,7 +3,7 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         int boats=0;
-        Arrays.sort(people);
+        Arrays.sort(people); 
         int i=0,j=people.length-1;
         while(i<=j){
             if((people[j]+people[i])<=limit){
