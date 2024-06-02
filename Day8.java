e3db59dba95a8ee789d48d3906acc33a7e4f9a88
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-06-02 12:10:06 +0530

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -7,7 +7,7 @@ class Solution {
             for (int c = 0; c < 9; c++) {
                 if (board[r][c] == '.') continue;
                 if (!seen.add(board[r][c] + "in row" + r) ||
-                    !seen.add(board[r][c] + "in col" + c) ||
+                    !seen.add(board[r][c] + "in col" + c) || 
                     !seen.add(board[r][c] + "in block" + r/3 + c/3)) {
                     return false;
                 }
