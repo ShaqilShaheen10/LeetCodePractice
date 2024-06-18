d0a6e4238cb9d086513de29e741bcf3536ebae14
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-06-18 14:29:30 +0530

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -4,7 +4,7 @@ class Solution {
     public int maxArea(int[] height) {
         int l=0;
         int r=height.length-1;
-        int m=0;
+        int m=0; 
         while(l<r){
             int c=Math.min(height[l],height[r])*(r-l);
             m=Math.max(c,m);
