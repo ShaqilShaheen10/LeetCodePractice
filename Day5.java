be680482062594be669e5db5130b869190da4f0c
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-06-19 14:19:12 +0530

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -94,7 +94,7 @@ class Solution {
  *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
+ *         this.val = val; 
  *         this.left = left;
  *         this.right = right;
  *     }
