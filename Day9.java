8a5e8f9b2c59d7cedc239ce6561a8a4cf7b3b1e7
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-07-03 10:54:33 +0530

    Update Day9.java

--- Day9.java
+++ Day9.java
@@ -31,21 +31,19 @@ class Solution {
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2. Range Sum of BST
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root==null){
