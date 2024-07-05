b741dbe7bf71e20181d82573c073c06d235a590e
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-07-05 11:09:35 +0530

    Update Day14.java

--- Day14.java
+++ Day14.java
@@ -39,7 +39,7 @@ class Solution {
     public boolean isUgly(int n) {
        if(n==0){
            return false;
-       }
+       } 
        for(int i=2;i<=6;i++){
            while(n%i==0){
                n=n/i;
