efafb573cd6adfa30727b2482b86bd6c13b6f26b
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-07-15 16:40:58 +0530

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -1,16 +1,14 @@
 1. Intersection of Two Linked Lists
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if(headA==null || headB==null) return null;
