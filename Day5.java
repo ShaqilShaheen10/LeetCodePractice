ba9e62087b83666f80ee30fd4331bf5274e7efa6
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-07-16 19:12:25 +0530

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -1,20 +1,18 @@
 1. Maximum Depth of a Binary Tree
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */ 
+ public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+  
 class Solution{
     public int maxDepth(TreeNode root){
        if(root==null) return 0; 
