74bff00b8cd71cc9bb9bd5f79ea8317f7eb02e4f
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-07-18 11:42:04 +0530

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -54,7 +54,7 @@ public class Solution {
        int r=0;
        for(int i=0;i<32;i++){
            if(((n>>i)&1)==1){
-               r++;
+               r++; 
            }
        }
        return r;
