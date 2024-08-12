b6522cfb60fdc94aa54ad8349a2c307e0f0e77fc
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-08-12 10:49:24 +0530

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -38,7 +38,7 @@ public class Solution {
     public int reverseBits(int n) {
         int r=0; 
         for(int i=0;i<32;i++){
-            r<<=1;
+            r<<=1; 
             r|=(n&1);
             n>>=1;
         }
