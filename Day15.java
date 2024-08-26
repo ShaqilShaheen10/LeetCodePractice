1286e4c02c60a0c619aa9dabe3cd62275a6dcca0
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-08-26 11:13:02 +0530

    Update Day15.java

--- Day15.java
+++ Day15.java
@@ -58,7 +58,7 @@ class Solution {
 public boolean isPerfectSquare(int num) {
         double sqr=Math.sqrt(num);
         int val=(int)sqr; 
-        if((val*val)==num){
+        if((val*val)==num){ 
             return true;
         }
         return false; 
