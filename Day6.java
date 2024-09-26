62b234f53291ff8ee06a39181d1eb59636191f52
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-09-26 10:47:59 +0530

    Update Day6.java

--- Day6.java
+++ Day6.java
@@ -15,7 +15,7 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
     List<Integer> ans=new ArrayList<>();
-    helper(root,ans);
+    helper(root,ans); 
     return ans;
     }
     private void helper(TreeNode node,List<Integer> ans){
