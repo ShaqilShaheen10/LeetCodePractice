29695af8de9b502a0c6aa13908f2ff623a316e73
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-10-06 22:51:34 +0530

    Update Day14.java

--- Day14.java
+++ Day14.java
@@ -6,7 +6,7 @@ class Solution {
         int[] a=new int[26];
         for(char c:magazine.toCharArray()){
             a[c-'a']++;
-        }
+        } 
         for(char c:ransomNote.toCharArray()){
             if(a[c-'a']==0) return false;
             a[c-'a']--;
