fff5ee00efe7cc8d4ea5d4a0823cdb106e54526d
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-10-27 22:04:15 +0530

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -30,7 +30,7 @@ class Solution{
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+  } 
   
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
