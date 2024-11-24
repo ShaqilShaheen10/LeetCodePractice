766b3b718b90decc0d4fa62a8e03b9249f921cf0
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-11-24 22:12:04 +0530

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -24,7 +24,7 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int start=0;
         int end=nums.length-1;
-        while(start<=end){
+        while(start<=end){ 
             int mid=start+(end-start)/2;
             if(nums[mid]==target) return mid;
             else if(nums[mid]>target) end=mid-1;
