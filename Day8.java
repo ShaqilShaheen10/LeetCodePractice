73b7b67140b670c3bc0671880afd06d16ef10e2f
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-11-25 11:07:25 +0530

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -3,7 +3,7 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<String> seen = new HashSet<>();
-        for (int r = 0; r < 9; r++) {
+        for (int r = 0; r < 9; r++) { 
             for (int c = 0; c < 9; c++) {
                 if (board[r][c] == '.') continue;
                 if (!seen.add(board[r][c] + "in row" + r) ||
