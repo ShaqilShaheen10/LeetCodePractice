9dec66f653d3e88d272c4d4b412c7713794c78e8
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-11-28 10:47:01 +0530

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -10,7 +10,7 @@ class Solution {
        if(i>=0)
         carry+=a.charAt(i--)-'0';
        if(j>=0)
-        carry+=b.charAt(j--)-'0'; 
+        carry+=b.charAt(j--)-'0';  
      s.append(carry%2);
      carry/=2; 
      }
