1791d056104cba72500cbc4d9dd963282d685f87
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2024-12-19 14:26:03 +0530

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -4,7 +4,7 @@ class Solution {
     public String addBinary(String a, String b) {
    StringBuilder s=new StringBuilder();
    int carry=0;
-   int i=a.length()-1;
+   int i=a.length()-1; 
    int j=b.length()-1;
    while(i>=0||j>=0||carry==1){
        if(i>=0)
