280f49b185d20de1a228e31083d5c940913b3a4a
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-01-19 13:39:47 +0530

    Update Day12.java

--- Day12.java
+++ Day12.java
@@ -10,7 +10,7 @@ class Solution {
         }
         for(int i=1;i<v.length;i++){
             if(v[i]==2){
-                dup=i;
+                dup=i; 
             }
             if(v[i]==0){
                 mis=i; 
