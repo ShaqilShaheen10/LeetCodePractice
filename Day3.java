dbe089d7e6eecba59ca0a3d97aba663956f97a13
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-02-10 18:56:07 +0530

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -67,7 +67,7 @@ class Solution {
     public int lengthOfLastWord(String s) {
      int count=0;
      for(int i=s.length()-1;i>=0;i--){ 
-         if(s.charAt(i)!=' '){
+         if(s.charAt(i)!=' '){ 
              count++;
          }
          else{
