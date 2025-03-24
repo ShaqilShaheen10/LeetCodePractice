aec93017fce8ee468a705bee13c43b1bbbff7e58
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-03-24 11:22:02 +0530

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -33,7 +33,7 @@ class Solution {
  
 class Solution { 
     public int addDigits(int num) {
-        if(num==0)return 0;
+        if(num==0)return 0; 
         else if(num%9==0)return 9;
         else return num%9;
     } 
