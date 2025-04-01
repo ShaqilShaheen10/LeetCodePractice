26000abff64fe322ff4223ab0d4d5cb1e0a847ff
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-04-01 13:26:00 +0530

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -69,7 +69,7 @@ class Solution{
 5. House Robber 
 
 class Solution {
-    public int rob(int[] nums) {
+    public int rob(int[] nums) { 
        int rob=0; 
        int norob=0;
        for(int i=0;i<nums.length;i++){
