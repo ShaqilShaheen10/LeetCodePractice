86a074f45f334b1a23ad108d43a8051eacce6237
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-04-05 12:42:36 +0530

    Update Day24.java

--- Day24.java
+++ Day24.java
@@ -9,7 +9,7 @@
 
   public int findTheWinner(int n, int k) { 
      int winner=0;
-     for(int i=1;i<=n;i++){ 
+     for(int i=1;i<=n;i++){  
         winner=(winner+k)%i;
      } 
      return winner+1; 
