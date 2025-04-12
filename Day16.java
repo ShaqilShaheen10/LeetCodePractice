8cf886464ad2869dfd4db712906ada8af064fa93
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-04-12 12:38:44 +0530

    Update Day16.java

--- Day16.java
+++ Day16.java
@@ -50,7 +50,7 @@ class Solution {
     public List<String> readBinaryWatch(int turnedOn) { 
         ArrayList<String> r=new ArrayList(); 
         for(int h=0;h<12;h++){
-            for(int m=0;m<60;m++){
+            for(int m=0;m<60;m++){ 
                 if(Integer.bitCount(h)+Integer.bitCount(m)==turnedOn){
                     if(m<10) r.add(String.format("%d:0%d",h,m));
                     else r.add(String.format("%d:%d",h,m));  
