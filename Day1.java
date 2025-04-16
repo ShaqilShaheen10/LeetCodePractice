73030060d7bf55ca92d729fa5525eb1e50b794ef
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-04-16 14:02:49 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -4,7 +4,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){ 
             for(int j=i+1;j<nums.length;j++){ 
-                if(nums[i]+nums[j]==target){
+                if(nums[i]+nums[j]==target){ 
                     int a[]={i,j};
                     return a; 
                 }
