5845ca8a0cddd62e8c62c50363adc11fc2d642ab
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-04-20 13:10:53 +0530

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -5,7 +5,7 @@ class Solution {
    StringBuilder s=new StringBuilder(); 
    int carry=0;
    int i=a.length()-1;  
-   int j=b.length()-1;
+   int j=b.length()-1; 
    while(i>=0||j>=0||carry==1){   
        if(i>=0) 
         carry+=a.charAt(i--)-'0'; 
