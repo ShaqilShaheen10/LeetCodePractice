2d1776eff740130036287623803f202d275babf5
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-04-21 12:37:41 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -41,7 +41,7 @@ class Solution {
       map.put('V',5); 
       map.put('X',10); 
       map.put('L',50);
-      map.put('C',100);
+      map.put('C',100); 
       map.put('D',500);
       map.put('M',1000);
     int result=map.get(s.charAt(s.length()-1));
