818088bc424b21dabc0b6e2972977c8d5e71666b
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-05-21 12:47:15 +0530

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -24,7 +24,7 @@ public class Solution {
 2. Power of Two 
  
 class Solution { 
-    public boolean isPowerOfTwo(int n) {
+    public boolean isPowerOfTwo(int n) { 
     return n>0 && (n&n-1)==0;
     } 
 }
