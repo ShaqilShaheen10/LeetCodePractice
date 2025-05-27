af076a644d9bf1558d8fa2bcafe29530a7050dcc
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-05-27 19:17:52 +0530

    Update Day15.java

--- Day15.java
+++ Day15.java
@@ -30,7 +30,7 @@ public class Solution extends GuessGame {
     class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         String c="123456789";
-        List<Integer> a=new ArrayList<>();
+        List<Integer> a=new ArrayList<>(); 
         for(int i=0;i<c.length();i++){ 
             for(int j=i+1;j<=c.length();j++){
                 int curr=Integer.parseInt(c.substring(i,j));
