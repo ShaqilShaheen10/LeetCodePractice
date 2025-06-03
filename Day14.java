dae3ddf93ae5bc004e976946c17bab54b3597f0a
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-06-03 12:58:50 +0530

    Update Day14.java

--- Day14.java
+++ Day14.java
@@ -66,21 +66,19 @@ class Solution {
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 5. Sum of Left Leaves
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
 class Solution {
     static int result;
     public int sumOfLeftLeaves(TreeNode root) {
