773ad05194d4cf05172ff6429b37b3c68a09f7c3
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-06-05 13:10:25 +0530

    Update Day16.java

--- Day16.java
+++ Day16.java
@@ -66,7 +66,7 @@ class Solution {
 class Solution {
     public int numSquares(int n) { 
         int sqrt=(int) Math.sqrt(n);  
-        if(sqrt*sqrt==n) return 1;
+        if(sqrt*sqrt==n) return 1; 
         while(n%4==0) n=n/4;
         if(n%8==7) return 4;
         for(int i=1;i*i<=n;i++){
