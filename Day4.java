212824f76289f2a30b71a906c7aa23569690c536
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-06-23 12:25:14 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -39,21 +39,19 @@ public class Solution{
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 3. Same Tree
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
 class Solution { 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null){ 
