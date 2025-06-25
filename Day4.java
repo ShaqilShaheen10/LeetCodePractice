102922258cdf3dccb8d979f47eae348942553012
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-06-25 12:32:20 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -30,7 +30,7 @@ public class Solution{
         long p=1; 
         for(int k=1;k<=R;k++){
             long n=p*(R-k+1)/k; 
-            r.add((int)n);
+            r.add((int)n); 
             p=n;
         }
         return r; 
