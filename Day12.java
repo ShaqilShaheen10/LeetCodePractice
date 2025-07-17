d0397c678b46086fdf387db8ebdaa0c5b256d426
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-07-17 14:03:02 +0530

    Update Day12.java

--- Day12.java
+++ Day12.java
@@ -28,7 +28,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(!set.add(nums[i])){
                 return true; 
-            }
+            } 
         }
         return false;
     }
