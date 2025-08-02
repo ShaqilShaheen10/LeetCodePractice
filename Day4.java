e7b2ece88c9fac3d4169bbe7c735c208694f2f64
Shaqil Shaheen <91945855+ShaqilShaheen10@users.noreply.github.com>
2025-08-02 14:07:20 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -55,7 +55,7 @@ public class Solution{
 class Solution { 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null){ 
-            return true;
+            return true; 
         }
         if(p==null || q==null || p.val!=q.val){  
             return false;
